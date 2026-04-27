1d9db57d63e51b989ee11db5bd4cae6c9abd36f7
omkutwal5050-wq <omkutwal5050@gmail.com>
2026-04-27 12:59:26 +0530

    Create indexing&slicilng.py

---
+++ indexing&slicilng.py
@@ -0,0 +1,14 @@
+text = "python"
+
+first_text = text[0]
+last_text = text[-1]
+string = text[0:2]
+length = len(text)
+up_text = text.upper()
+
+print("Original text:", text)
+print("First character:", first_text)
+print("Last character:", last_text)
+print("String (0:2):", string)
+print("Length:", length)
+print("Uppercase:", up_text)
