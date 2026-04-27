867058e5061f2b7063b8f2e7575d6be5917e036c
omkutwal5050-wq <omkutwal5050@gmail.com>
2026-04-27 12:56:53 +0530

    Create data conversion.py

---
+++ data conversion.py
@@ -0,0 +1,9 @@
+try:
+    x = input("Enter value: ")
+    print("Integer:", int(x))
+    print("Float:", float(x))
+    print("String:", str(x))
+    print("Boolean:", bool(x))
+
+except:
+    print("Invalid conversion")
