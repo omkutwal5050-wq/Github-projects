94264c02e69f6aceba9cfb77165b9259ce3846c7
omkutwal5050-wq <omkutwal5050@gmail.com>
2026-04-27 13:01:13 +0530

    Create builtinfunction.py

---
+++ builtinfunction.py
@@ -0,0 +1,10 @@
+num = [10, 20, 30, 40, 50]
+
+length = len(num)
+total = sum(num)
+maximum = max(num)
+
+print("List:", num)
+print("Length:", length)
+print("Sum:", total)
+print("Maximum:", maximum)
