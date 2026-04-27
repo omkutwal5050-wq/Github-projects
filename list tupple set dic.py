be22d69e63508fc7ddb66e2e28cabfb10b872146
omkutwal5050-wq <omkutwal5050@gmail.com>
2026-04-27 13:00:37 +0530

    Create list tupple set dic.py

---
+++ list tupple set dic.py
@@ -0,0 +1,13 @@
+my_list = [10, 20, 30, 40]
+my_tuple = (1, 2, 3, 4)
+my_set = {5, 10, 15, 20}
+my_dict = {
+    101: "Amit",
+    102: "Neha",
+    103: "Rahul"
+}
+
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Set:", my_set)
+print("Dictionary:", my_dict)
